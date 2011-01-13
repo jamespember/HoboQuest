d2ebd3166dc70d94ecd12c777ac9a2473194ac5a
mogel <vigge19@gmail.com>
2011-01-13 16:50:20 +0100

    updated item test with type check

--- test/test_item.h
+++ test/test_item.h
@@ -10,10 +10,12 @@ void test_item() {
 	Item i1("Item 1, no value");
 	assert(i1.name() == "Item 1, no value");
 	assert(i1.value() == 0);
+	assert(i1.type() == "item");
 
 	Item i2("2", 100);
 	assert(i2.value() == 100);
 	assert(i2.name() == "2");
+	assert(i2.type() == "item");
 }
 
 #endif
