ea18e959469c16433fa4d068804937e9f503fca7
shark <jostjernberg@gmail.com>
2011-01-13 16:13:14 +0100

    container methods for item handling

--- container.h
+++ container.h
@@ -3,17 +3,29 @@
 
 #include <string>
 #include <map>
-#include "item.h"
+//#include "item.h"
+
+
 
 namespace hoboquest {
+	struct Item {
+		
+	};
+
 	class Container {
 		protected:
-			std::map<std::string, *Item> items;
-
+			std::map<std::string, Item> items;
 		public:
 			bool pickup(Item a);
-			Item & has(std::string name);
-			Item & drop(std::string name);
+			Item & has(std::string name) {
+				return & *this->items.find(name);
+			}
+			Item & drop(std::string name) {
+				Item & it = has(name);
+				if (it != NULL)
+					items.erase(name);
+				return it;
+			}
 	};
 }
 
