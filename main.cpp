a3dbc39d71f2699e3c5c83afc8ed80af13a58f8f
mogel <vigge19@gmail.com>
2011-01-13 16:49:59 +0100

    removed test code from main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <stdlib.h>
 #include <iostream>
-#include "hoboquest/container.h"
-#include "hoboquest/item.h"
+#include "hoboquest/game.h"
 
-using hoboquest::Container;
-using hoboquest::Item;
+using hoboquest::Game;
 
 int main(int argc, char** argv) {
-	//Game g(stdin, stdout);
-	Container c();
-	Item it("key");
-	c.pickup(it);
-	std::cout << (c.has("key") == it) << std::endl;
-	std::cout << (c.drop("key") == it) << std::endl;
+	Game g(stdin, stdout);
 	return (EXIT_SUCCESS);
 }
