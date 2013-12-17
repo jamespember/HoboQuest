3af08e4540b01dc971dcadc0dd8bb22877a1c69e
Victor Hallberg <vigge19@gmail.com>
2013-12-17 17:10:30 +0100

    Add actor and container

---
+++ src/actor.hpp
@@ -0,0 +1,43 @@
+#ifndef HOBO_ACTOR
+#define HOBO_ACTOR
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "container.hpp"
+#include "area.hpp"
+#include "item.hpp"
+
+namespace hoboquest {
+  class Actor : public Container {
+    protected:
+      string _name;
+      bool _active;
+      std::unordered_map<string, int> _stats;
+      std::shared_ptr<Area> _location;
+
+    public:
+      Actor(string name) : _name(name) : _active(false) {
+        _stats["hp"] = 1;
+        _stats["damage"] = 1;
+        _stats["capacity"] = 0;
+      }
+
+      on_enter(std::shared_ptr<Area> area) {
+      }
+
+      on_exit(std::shared_ptr<Area> area) {
+      }
+
+      move_to(std::shared_ptr<Area> area) {
+        if (*area)
+          on_exit(_location);
+        _location = area;
+        on_enter(_location);
+      }
+      
+  };
+} /* hoboquest  */ 
+
+#endif
--- src/container.hpp
+++ src/container.hpp
@@ -2,25 +2,26 @@
 #define HOBO_CONTAINER
 
 #include <string>
-#include <map>
-#include "item.h"
-
-using hoboquest::Item;
+#include <unordered_map>
+#include "item.hpp"
 
 namespace hoboquest {
-
 	class Container {
 		protected:
-			std::map<std::string, *Item> items;
+			std::unordered_map<std::string, Item> _items;
+
 		public:
-			Container() { }
-			bool pickup(std::string name, Item a) {
-				items.insert(name, *a);
+			Container() {}
+
+			bool pickup(Item item) {
+				_items.insert(item->name, item);
 			}
+
 			Item & has(std::string name) {
 				return & *this->items.find(name);
 			}
-			Item & drop(std::string name) {
+
+			Item drop(std::string name) {
 				Item & it = has(name);
 				if (it != NULL)
 					items.erase(name);
