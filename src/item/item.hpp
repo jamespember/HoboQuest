116eb8d762db977c674ffdbe950ae59fefde4306
Johan Stjernberg <jostjernberg@gmail.com>
2013-12-20 18:34:45 +0100

    Feature; Added consumable and equippable item classes.

--- src/item/item.hpp
+++ src/item/item.hpp
@@ -5,16 +5,8 @@
 
 namespace hoboquest {
 	class Item {
-		public:
-				enum Type {
-					Equippable,
-					Container,
-					Consumable
-				};
-
 		protected:
 			std::string _name;
-			Item::Type _type;
 			unsigned _value, _weight;
 
 		public:
@@ -22,7 +14,6 @@ namespace hoboquest {
 			Item(std::string name, unsigned value);
 			Item(std::string name, unsigned value, unsigned weight);
 
-			const Item::Type & type()		const;
 			const std::string & name()	const;
 			unsigned value()						const;
 			unsigned weight()						const;
--- src/test/item.hpp
+++ src/test/item.hpp
@@ -3,6 +3,8 @@
 
 #include <cassert>
 #include "../item/item.hpp"
+#include "../item/equippable.hpp"
+#include "../item/consumable.hpp"
 
 using namespace hoboquest;
 
@@ -11,16 +13,34 @@ void test_item() {
 	assert(i1.name() == "Item 1, no value");
 	assert(i1.value() == 1);
 	assert(i1.weight() == 1);
-	assert(i1.type() == Item::Type::Equippable);
 
 	Item i2("2", 100);
 	assert(i2.value() == 100);
 	assert(i2.name() == "2");
-	assert(i2.type() == Item::Type::Equippable);
 
   Item custom("Custom");
   assert(custom.value() == 1);
   assert(custom.weight() == 1);
+  assert(custom.is_equippable() == false);
+  assert(custom.is_container() == false);
+  assert(custom.is_consumable() == false);
+  
+  Equippable sword("Sword");
+  assert(sword.is_equippable() == true);
+  assert(sword.is_container() == false);
+  assert(sword.is_consumable() == false);
+  
+  /*
+  Container bag("Bag");
+  assert(bag.is_equippable() == false); // TODO: should be equippable too?
+  assert(bag.is_container() == true);
+  assert(bag.is_consumable() == false);
+	*/
+	
+  Consumable beer("Beer");
+  assert(beer.is_equippable() == false);
+  assert(beer.is_container() == false);
+  assert(beer.is_consumable() == true);
 }
 
 #endif
