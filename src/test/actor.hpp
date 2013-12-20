663b2f1365cf2d63c3718c90d18d39c4004e160f
Johan Stjernberg <jostjernberg@gmail.com>
2013-12-20 14:14:16 +0100

    Added actor tests.

--- src/test/actor.hpp
+++ src/test/actor.hpp
@@ -11,18 +11,23 @@ using namespace hoboquest;
 void test_actor() {
 	auto area = make_shared<Area>("House", "A small wooden house");
 	auto actor = make_shared<Actor>("Thief");
-
 	area->add_actor(actor);
-	// cout << area->has_actor(actor->get_name()) << endl;
+	
+	// Verify actor name and that area has the actor
+	assert(actor->get_name() == "Thief");
+	assert(area->has_actor(actor->get_name()) == true);
+	assert(area->has_actor("Hufflepuff") == false);
+	
+	// Verify fetching actor works and that name changes are reflected globally
 	auto test = area->get_actor(actor->get_name());
-
 	actor->set_name("Dirty thief");
+	assert(actor->get_name() == "Dirty thief");
+	assert(test->get_name() == actor->get_name());
 
-	// cout << "Area " << area->get_name() << " has actor " << test->get_name();
-	// cout << endl;
-
+	// Verify that removing actors from areas work
 	area->remove_actor(actor->get_name());
-	// cout << area->has_actor(actor->get_name()) << endl;
+	assert(area->has_actor(actor->get_name()) == false);
+	
 }
 
 #endif
