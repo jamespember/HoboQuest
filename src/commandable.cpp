3d65c92e32864494662b16f33f722b078c1a245e
Victor Hallberg <vigge19@gmail.com>
2013-12-28 22:40:38 +0100

    Remove ::iterator version of get_command_iterator as it wasn't needed and caused segfaults

--- src/commandable.cpp
+++ src/commandable.cpp
@@ -3,8 +3,11 @@
 #include "command.hpp"
 #include "player.hpp"
 
+#include <memory>
 #include <list>
 #include <string>
+#include <cassert>
+#include <iostream>
 
 namespace hoboquest {
 
@@ -21,25 +24,21 @@ namespace hoboquest {
   Commandable::container_type::const_iterator
     Commandable::get_command_iterator(const std::string &name) const {
     Commandable::container_type::const_iterator it = _commands.begin(), itend = _commands.end();
+    if (_commands.empty())
+      return itend;
     for (; it != itend; it++)
       if ((*it)->responds_to(name))
         return it;
     return itend;
   }
 
-  Commandable::container_type::iterator
-    Commandable::get_command_iterator(const std::string &name) {
-    // Get const_iterator to command and use erase() to get a normal iterator for it.
-    auto it = Commandable::get_command_iterator(name);
-    return _commands.erase(it, it);
-  }
-
   // Removes (last) command whose name or alias matches.
   bool Commandable::remove_command(const std::string &name) {
     auto it = get_command_iterator(name);
-    if (it != _commands.end())
-      return _commands.erase(it), true;
-    return false;
+    if (it == _commands.end())
+      return false;
+    _commands.erase(it);
+    return true;
   }
 
   bool Commandable::has_command(const std::string &name) const {
@@ -48,7 +47,7 @@ namespace hoboquest {
 
   bool Commandable::try_execute(const std::string &name, Player &player,
       std::list<std::string> &args) {
-    auto it = get_command_iterator(name);
+    Commandable::container_type::const_iterator it = get_command_iterator(name);
     if (it == _commands.end())
       return false;
     return (*it)->execute(player, args);
