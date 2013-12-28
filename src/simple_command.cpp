a635e8b7e5388cb462a34e6f7640285ff339ec52
Victor Hallberg <vigge19@gmail.com>
2013-12-28 20:02:13 +0100

    Add SimpleCommand class which simplifies command definitions by allowing the execution function body to be specified in the constructor

---
+++ src/simple_command.cpp
@@ -0,0 +1,22 @@
+#include "simple_command.hpp"
+
+#include "command.hpp"
+#include "player.hpp"
+
+#include <vector>
+#include <string>
+
+namespace hoboquest {
+
+  SimpleCommand::SimpleCommand(const std::string &name, const std::string &alias, const command_function &executor) :
+    Command(name, alias), _executor(executor) {}
+  SimpleCommand::SimpleCommand(const std::string &name, const command_function &executor) :
+    Command(name), _executor(executor) {}
+
+  SimpleCommand::~SimpleCommand() {}
+
+  bool SimpleCommand::execute(Player &player, std::list<std::string> &args) {
+    return _executor(player, args);
+  }
+
+} /* hoboquest */
---
+++ src/simple_command.hpp
@@ -0,0 +1,29 @@
+#ifndef HOBO_SIMPLE_COMMAND
+#define HOBO_SIMPLE_COMMAND
+
+#include "command.hpp"
+
+#include <list>
+#include <string>
+#include <functional>
+
+namespace hoboquest {
+  class Player;
+
+  typedef std::function<bool(Player&, std::list<std::string>&)> command_function;
+
+  class SimpleCommand : public Command {
+    protected:
+      command_function _executor;
+
+    public:
+      SimpleCommand(const std::string &name, const std::string &alias, const command_function &executor);
+      SimpleCommand(const std::string &name, const command_function &executor);
+      virtual ~SimpleCommand();
+
+      bool execute(Player &player, std::list<std::string> &args);
+
+  };
+} /* hoboquest */ 
+
+#endif
