b5ec1970fc70b6dc824ccd1f202b9dbabcb4c8a4
Johan Stjernberg <jostjernberg@gmail.com>
2014-01-30 21:26:16 +0100

    Refactoring; tabs to space fix.

--- src/game/cat_quest.hpp
+++ src/game/cat_quest.hpp
@@ -17,21 +17,21 @@ namespace hoboquest {
       CatQuest(Engine &engine,
           PtrMap<Area> &areas, shared_ptr<Actor> actor) :
       Quest("cat_quest", "The Cat Quest", engine), _areas(areas), _actor(actor) {
-					place_cats();
+        place_cats();
       }
       
       void place_cats() {
-      	vector<shared_ptr<Equippable>> cats;
-      	vector<string> cat_ids = {"mjau", "cow", "milk", "catwoman", "curt"};
-      	vector<string> cat_names = {"Mr Mjau", "Mr Cow", "Mrs Milk", "Catwoman", "Curt"};
-      	vector<string> cat_areas = {"market", "floor1", "shelter", "alley", "police_station"};
-      	
-      	for (int x = 0; x < 5; x++) {
-		    	auto cat = make_shared<Equippable>(cat_ids[x], cat_names[x]);
-		      cat->set_weight(5);
-		      cat->set_value(10);
-		      _areas.get(cat_areas[x])->add_item(cat);
-      	}
+        vector<shared_ptr<Equippable>> cats;
+        vector<string> cat_ids = {"mjau", "cow", "milk", "catwoman", "curt"};
+        vector<string> cat_names = {"Mr Mjau", "Mr Cow", "Mrs Milk", "Catwoman", "Curt"};
+        vector<string> cat_areas = {"market", "floor1", "shelter", "alley", "police_station"};
+        
+        for (int x = 0; x < 5; x++) {
+          auto cat = make_shared<Equippable>(cat_ids[x], cat_names[x]);
+          cat->set_weight(5);
+          cat->set_value(10);
+          _areas.get(cat_areas[x])->add_item(cat);
+        }
       }
 
       void on_start() {
